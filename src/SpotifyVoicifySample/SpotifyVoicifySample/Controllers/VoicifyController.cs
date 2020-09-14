b04844cb3d18241c23aa1e7ee7a1132f8b0e8251
Alex Dunn <alexander.dunn@outlook.com>
2020-09-14 10:26:29 -0400

    initial project

---
+++ src/SpotifyVoicifySample/SpotifyVoicifySample/Controllers/VoicifyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyVoicifySample.Controllers
+{
+    [Route("api/[controller]")]
+    public class VoicifyController : Controller
+    {
+        [HttpPost("HandleRequest")]
+        public async Task<IActionResult> HandleRequest()
+        {
+            return Ok();
+        }
+    }
+}
